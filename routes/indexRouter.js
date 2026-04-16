0ca851ae6b01687a4a39ee0f08b778428145cfd6
Michael <100490812+DebSol11@users.noreply.github.com>
2026-04-17 00:02:59 +0200

    1,2,3 Wir sind nicht high

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,19 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 indexRouter.get("/", (req, res) => res.send("All books"));
 indexRouter.get("/:indexId", (req, res) => {
     const { indexId } = req.params;
